c6d921b6038ac05e2c8f70c6de1349476812a61e
Daniel King <daniel.zidan.king@gmail.com>
2015-04-26 23:12:38 -0400

    change example back

--- class-presentation-two/example.c
+++ class-presentation-two/example.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+float average(float * array, int length);
 
+int main (int argc, char ** argv) {
   float array [100] = { 10000000
                        , 0.09, 0.09, 0.09, 0.09, 0.09
                        , 0.09, 0.09, 0.09, 0.09, 0.09
@@ -24,32 +26,19 @@
                        , 0.09, 0.09, 0.09
                        , 10000000};
 
-float average(float * array, int length);
+  float avg = average(array, 100);
 
-int main (int argc, char ** argv) {
-
-  int length = 100;
-  float sum = 0;
-  /* for (int i = 0; i < length; ++i) { */
-  /*   sum = sum + *(array + i); */
-  /* } */
-  sum = sum + array[0];
-  /* printf("%f\n", sum); */
-  /* return sum / length; */
-  /* float avg = average(array, 100); */
-  float avg = sum / length;
-
-  /* printf("%f\n", avg); */
+  printf("%f\n", avg);
 
   return 0;
 }
 
 
-/* float average(float * array, int length) { */
-/*   float sum = 0; */
-/*   for (int i = 0; i < length; ++i) { */
-/*     sum = sum + *(array + i); */
-/*   } */
-/*   printf("%f\n", sum); */
-/*   return sum / length; */
-/* } */
+float average(float * array, int length) {
+  float sum = 0;
+  for (int i = 0; i < length; ++i) {
+    sum = sum + *(array + i);
+  }
+  printf("%f\n", sum);
+  return sum / length;
+}
