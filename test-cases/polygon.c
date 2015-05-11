48a6ad88d7f31642114f54869e7315fcca534a20
Hannah Blumberg <hblumberg0@gmail.com>
2015-05-10 17:29:53 -0700

    Updated test case (polygon).

--- test-cases/polygon.c
+++ test-cases/polygon.c
@@ -2,33 +2,51 @@
 #include <stdio.h>
 
 /*
- *http://leafea.s3-website-us-east-1.amazonaws.com/writing/formula.pdf
+ * http://leafea.s3-website-us-east-1.amazonaws.com/writing/formula.pdf
  * We are given a point X and a simple polygon P with N vertices A1, A2, ..., An. We want to determine if X is inside P (exluding boundaries).
  */
 
 int isInside(float x, float y, int n, float xs[], float ys[]) {
   float angle = 0;
-  float ux = 0;
-  float uy = 0;
-  float vx = 0;
-  float vy = 0;  
-  for (int i = 0; i < n-1; i++) {
+  float ux, uy, vx, vy;
+  for (int i = 0; i < n; i++) {
     ux = xs[i] - x;
     uy = ys[i] - y;
-    vx = xs[i+1] - x;
-    vy = ys[i+1] - y;
-    angle += atan(uy/ux) - atan(vy/vx);
+    vx = x - xs[(i+1)%n];
+    vy = y - ys[(i+1)%n];
+    angle += atan2f(uy,ux) - atan2f(vy,vx);
   }
-  if (fabsf(angle) < 2 * M_PI) {
+  if (fabsf(angle) < 2*M_PI) {
+    return 1;
+  }
+  return 0;
+}
+
+int isInsideImp(float x, float y, int n, float xs[], float ys[]) {
+  float angle = 0;
+  float ux, uy, vx, vy;
+  for (int i = 0; i < n; i++) {
+    ux = xs[i] - x;
+    uy = ys[i] - y;
+    vx = x - xs[(i+1)%n];
+    vy = y - ys[(i+1)%n];
+    angle += (ux*vx + uy*vy)/(sqrt(pow(ux,2) + pow(uy,2)) * sqrt(pow(vx,2) + pow(vy,2)));
+  }
+  if (fabsf(angle) < 2*M_PI) {
     return 1;
   }
   return 0;
 }
 
 int main(void) {
-  float xs[] = {-10000000.0, 0.0, 10000000.0};
-  float ys[] = {0.0, 0.01, 0.0};
-  int result = isInside(0.0, 0.0, 3, xs, ys);
-  printf("Answer: %d\n", result);
+  float xs[] = {-0.00001, 0, 0.00001};
+  float ys[] = {0, 0.00001, 0};
+  int n = 3;
+  float testx = 0;
+  float testy = 0.0000101;
+  int result = isInsideImp(testx, testy, n, xs, ys);
+  printf("Imprecise Algorithm: %d\n", result);
+  result = isInside(testx, testy, n, xs, ys);
+  printf("More Precise Algorithm: %d\n", result);
   return 0;
 }
