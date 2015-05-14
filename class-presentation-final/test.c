2b737b39717e1979b32be337d886e23bfb575b65
Daniel King <daniel.zidan.king@gmail.com>
2015-05-14 09:54:32 -0400

    more fixes

--- class-presentation-final/test.c
+++ class-presentation-final/test.c
@@ -4,20 +4,23 @@
 #define TEST_CASE_COUNT 9
 
 double test_cases [TEST_CASE_COUNT * 3] =
-  { 1, 1, 1,
-    10, 10, 10,
-    1e10, 1e10, 1e10,
-    1e-10, 1e-10, 1e-10,
+  { 1, 10, 1,
+    10, 100, 10,
+    1e10, 1e11, 1e10,
+    1e-10, 1e-4, 1e-10,
     1e10, 1e10, 1e-10,
     1e10, 1e-10, 1e-10,
-    -10, -10, 10,
+    -10, -50, 10,
     -1e10, -1e10, 1e10,
     -1e-10, -1e-10, 1e-10 };
 
 int main(int argc, char ** argv) {
-  for (int i = 0 ; i < TEST_CASE_COUNT ; i += 3) {
-    f1(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
-    f2(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
+  int i;
+  for (i = 0 ; i < TEST_CASE_COUNT * 3 ; i += 3) {
+    double first = f1(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
+    double second = f2(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
+    printf("For test case %d, the results were: %f, %f\n", i / 3, first, second);
   }
+  printf("Ran %d test cases.\n", i / 3);
   return 0;
 }
