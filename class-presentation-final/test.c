34f4e1647c57ba26426bf98f898c737e828bd1da
Daniel King <daniel.zidan.king@gmail.com>
2015-05-14 11:12:59 -0400

    more changes to test cases

--- class-presentation-final/test.c
+++ class-presentation-final/test.c
@@ -2,22 +2,24 @@
 #include "challenge.h"
 
 #define TEST_CASE_COUNT 14
+#define SHOW_REFERENCE 0
 
 double test_cases [TEST_CASE_COUNT * 3] =
-  { 100           , -8356218543e23    , -321432,  // 0
-    -21343243e5   , -3421893e57       , 729e22,   // 1
-    -10           , -50               , 10,       // 2
-    -1e10         , -1e10             , 1e10,     // 3
-    -1e-10        , -1e-10            , 1e-10,    // 4
-    1e-10         , 1e-4              , 1e-10,    // 5
-    1             , 3                 , 2.25,     // 6
-    1             , 10                , 1,        // 7
-    10            , 100               , 10,       // 8
-    1e10          , 1e11              , 1e10,     // 9
-    1e10          , 1e10              , 1e-10,    // 10
+  { 100           , -8356218543e201   , -321432,  // 0
+    100           , -8356218543e23    , -321432,  // 1
+    -21343243e5   , -3421893e57       , 729e22,   // 2
+    -10           , -50               , 10,       // 3
+    -1e10         , -1e10             , 1e10,     // 4
+    -1e-10        , -1e-10            , 1e-10,    // 5
+    3.214323e-201 , 5e-100            , 1.32423,  // 6
+    1e-10         , 1e-4              , 1e-10,    // 7
+    1             , 3                 , 2.25,     // 8
+    1             , 10                , 1,        // 9
+    1e10          , 1e11              , 1e10,     // 10
     -342e57       , 1e10              , 1e-10,    // 11
     98045376e102  , 432789658e201     , 42e-42,   // 12
-    4328973e5     , 432789658e201     , 2134e2    // 13
+    4328973e5     , 432789658e201     , 2134e2,   // 13
+    4328973e202   , 432789658e201     , 2134e2    // 14
   };
 
 int main(int argc, char ** argv) {
@@ -25,8 +27,13 @@ int main(int argc, char ** argv) {
   for (i = 0 ; i < TEST_CASE_COUNT * 3 ; i += 3) {
     double ref = reference(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
     double soln = solution(test_cases[i + 0], test_cases[i + 1], test_cases[i + 2]);
-    printf("For test case %.2d, the reference implementation was: %.16e\n", i / 3, ref);
-    printf("                            your solution produced: %.16e\n", soln);
+
+    if (SHOW_REFERENCE) {
+      printf("For test case %2d, the reference implementation was: %.16e\n", i / 3, ref);
+      printf("                            your solution produced: %.16e\n", soln);
+    } else {
+      printf("For test case %2d, your solution produced: %.16e\n", i / 3, soln);
+    }
   }
   printf("Ran %d test cases.\n", i / 3);
   return 0;
